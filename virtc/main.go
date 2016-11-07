45a8bdc7acf9458aa5f5047d28aac2cc45666312
Samuel Ortiz <sameo@linux.intel.com>
2016-11-07 01:16:02 +0100

    virtc: Support glog and urfave/cli at the same time
    It's not pretty, but until urfave/cli supports glog properly, we need to
    explicitly parse the glog flags.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- virtc/main.go
+++ virtc/main.go
@@ -398,8 +398,62 @@ var statusPodCommand = cli.Command{
 	},
 }
 
+func glogFlagShim(fakeVals map[string]string) {
+	flag.VisitAll(func(fl *flag.Flag) {
+		if val, ok := fakeVals[fl.Name]; ok {
+			fl.Value.Set(val)
+		}
+	})
+}
+
+func glogShim(c *cli.Context) {
+	_ = flag.CommandLine.Parse([]string{})
+	glogFlagShim(map[string]string{
+		"v":                fmt.Sprint(c.Int("v")),
+		"logtostderr":      fmt.Sprint(c.Bool("logtostderr")),
+		"stderrthreshold":  fmt.Sprint(c.Int("stderrthreshold")),
+		"alsologtostderr":  fmt.Sprint(c.Bool("alsologtostderr")),
+		"vmodule":          c.String("vmodule"),
+		"log_dir":          c.String("log_dir"),
+		"log_backtrace_at": c.String("log_backtrace_at"),
+	})
+}
+
+var glogFlags = []cli.Flag{
+	cli.IntFlag{
+		Name: "v", Value: 0, Usage: "log level for V logs",
+	},
+	cli.BoolFlag{
+		Name: "logtostderr", Usage: "log to standard error instead of files",
+	},
+	cli.IntFlag{
+		Name:  "stderrthreshold",
+		Usage: "logs at or above this threshold go to stderr",
+	},
+	cli.BoolFlag{
+		Name: "alsologtostderr", Usage: "log to standard error as well as files",
+	},
+	cli.StringFlag{
+		Name:  "vmodule",
+		Usage: "comma-separated list of pattern=N settings for file-filtered logging",
+	},
+	cli.StringFlag{
+		Name: "log_dir", Usage: "If non-empty, write log files in this directory",
+	},
+	cli.StringFlag{
+		Name:  "log_backtrace_at",
+		Usage: "when logging hits line file:N, emit a stack trace",
+		Value: ":0",
+	},
+}
+
 func main() {
 	flag.Parse()
+
+	cli.VersionFlag = cli.BoolFlag{
+		Name:  "version",
+		Usage: "print the version",
+	}
 
 	virtc := cli.NewApp()
 	virtc.Name = "VirtContainers CLI"
@@ -419,6 +473,12 @@ func main() {
 				statusPodCommand,
 			},
 		},
+	}
+
+	virtc.Flags = append(virtc.Flags, glogFlags...)
+	virtc.Action = func(c *cli.Context) error {
+		glogShim(c)
+		return nil
 	}
 
 	err := virtc.Run(os.Args)
