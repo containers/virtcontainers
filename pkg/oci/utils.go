1ea875ed0166862a58f43aa1de090484f65f5d85
Samuel Ortiz <sameo@linux.intel.com>
2017-01-31 00:05:39 +0100

    pkg/oci: Initial pod command implementation
    With arguments, environment variables, and CWD.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- pkg/oci/utils.go
+++ pkg/oci/utils.go
@@ -18,11 +18,29 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	vc "github.com/containers/virtcontainers"
 	spec "github.com/opencontainers/runtime-spec/specs-go"
 )
+
+func cmdEnvs(spec spec.Spec, envs []vc.EnvVar) []vc.EnvVar {
+	for _, env := range spec.Process.Env {
+		kv := strings.Split(env, "=")
+		if len(kv) < 2 {
+			continue
+		}
+
+		envs = append(envs,
+			vc.EnvVar{
+				Var:   kv[0],
+				Value: kv[1],
+			})
+	}
+
+	return envs
+}
 
 // PodConfig converts an OCI compatible runtime configuration file
 // to a virtcontainers pod configuration structure.
@@ -42,11 +60,18 @@ func PodConfig(bundlePath, cid, console string, interactive bool) (*vc.PodConfig
 
 	rootfs := filepath.Join(bundlePath, ocispec.Root.Path)
 
+	cmd := vc.Cmd{
+		Args:    ocispec.Process.Args,
+		Envs:    cmdEnvs(ocispec, []vc.EnvVar{}),
+		WorkDir: ocispec.Process.Cwd,
+	}
+
 	containerConfig := vc.ContainerConfig{
 		ID:          cid,
 		RootFs:      rootfs,
 		Interactive: interactive,
 		Console:     console,
+		Cmd:         cmd,
 	}
 
 	podConfig := vc.PodConfig{
