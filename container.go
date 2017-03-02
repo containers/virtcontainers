eee4285c25827c03a73ee0903ad4ab561cc08772
Sebastien Boeuf <sebastien.boeuf@intel.com>
2017-03-01 17:18:53 -0800

    hyperstart: Move VM registration to startAgent()
    Runtime implementations such as OCI can expect to retrieve a PID
    corresponding to the running process at the time they call into
    create() function.
    In our case, this means that we have to provide the PID of the shim
    we have just started. But we also expect to get a token from the proxy
    before we can start the shim, otherwise it won't be authorized by the
    proxy. That is why we move the registration of the VM to the function
    startAgent() which is called by the CreatePod() API.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- container.go
+++ container.go
@@ -30,6 +30,10 @@ import (
 type Process struct {
 	Token string
 	Pid   int
+
+	// TODO: Remove these fields when new proxy protocol is ready.
+	Stdio  uint64
+	Stderr uint64
 }
 
 // ContainerStatus describes a container status.
@@ -111,11 +115,15 @@ func (c *Container) GetPid() int {
 func (c *Container) SetPid(pid int) error {
 	c.process.Pid = pid
 
-	if err := c.pod.storage.storeContainerProcess(c.podID, c.id, c.process); err != nil {
-		return err
-	}
+	return c.storeProcess()
+}
 
-	return nil
+func (c *Container) storeProcess() error {
+	return c.pod.storage.storeContainerProcess(c.podID, c.id, c.process)
+}
+
+func (c *Container) fetchProcess() (Process, error) {
+	return c.pod.storage.fetchContainerProcess(c.podID, c.id)
 }
 
 // fetchContainer fetches a container config from a pod ID and returns a Container.
@@ -308,11 +316,6 @@ func (c *Container) start() error {
 		}
 	}
 
-	err = c.pod.agent.startAgent()
-	if err != nil {
-		return err
-	}
-
 	err = c.pod.agent.startContainer(*c.pod, *(c.config))
 	if err != nil {
 		c.stop()
@@ -338,11 +341,6 @@ func (c *Container) stop() error {
 	}
 
 	err = state.validTransition(StateRunning, StateStopped)
-	if err != nil {
-		return err
-	}
-
-	err = c.pod.agent.startAgent()
 	if err != nil {
 		return err
 	}
@@ -375,11 +373,6 @@ func (c *Container) enter(cmd Cmd) error {
 		return fmt.Errorf("Container not running, impossible to enter")
 	}
 
-	err = c.pod.agent.startAgent()
-	if err != nil {
-		return err
-	}
-
 	err = c.pod.agent.exec(*c.pod, *c, cmd)
 	if err != nil {
 		return err
@@ -396,11 +389,6 @@ func (c *Container) kill(signal syscall.Signal) error {
 
 	if state.State != StateRunning {
 		return fmt.Errorf("Container not running, impossible to signal the container")
-	}
-
-	err = c.pod.agent.startAgent()
-	if err != nil {
-		return err
 	}
 
 	err = c.pod.agent.killContainer(*c.pod, *c, signal)
