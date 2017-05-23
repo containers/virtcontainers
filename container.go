b25540eb940ac0768dcdf9c8bb923d895c7047b7
Sebastien Boeuf <sebastien.boeuf@intel.com>
2017-05-23 09:26:03 -0700

    container: Get pod handler from any container
    It can be very useful to be able to interact with the pod related
    to the current container, by retrieving the pod handler inside the
    container structure.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- container.go
+++ container.go
@@ -103,6 +103,11 @@ func (c *Container) ID() string {
 	return c.id
 }
 
+// Pod returns the pod handler related to this container.
+func (c *Container) Pod() *Pod {
+	return c.pod
+}
+
 // Process returns the container process.
 func (c *Container) Process() Process {
 	return c.process
