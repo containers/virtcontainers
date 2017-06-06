2405c8845d4b4c768ef1533cf5a4db0ae8299952
Archana Shinde <archana.m.shinde@intel.com>
2017-06-06 01:13:50 -0700

    storage: Add function to get the underlying device and mount point
    Add helper function in preparation for using devicemapper block
    device. This function is used for getting the device major and
    minor number for the underlying device containing the file
    and the mountpoint of the mount containing the file.
    
    We will be using this for for getting the device major-minor associated
    with the container rootfs and the mountPoint of the rootfs.
    
    We will eventually use the device to verify if it is a block device
    associated with devicemapper.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- mount.go
+++ mount.go
@@ -16,5 +16,78 @@
 
 package virtcontainers
 
+import (
+	"fmt"
+	"path/filepath"
+	"syscall"
+)
+
 // These mounts need to be created by the agent within the VM
 var systemMounts = []string{"/proc", "/dev", "/dev/pts", "/dev/shm", "/dev/mqueue", "/sys", "/sys/fs/cgroup"}
+
+func major(dev uint64) int {
+	return int((dev >> 8) & 0xfff)
+
+}
+
+func minor(dev uint64) int {
+	return int((dev & 0xff) | ((dev >> 12) & 0xfff00))
+}
+
+type device struct {
+	major      int
+	minor      int
+	mountPoint string
+}
+
+// getDeviceForPath gets the underlying device containing the file specified by path.
+// The device type constitutes the major-minor number of the device and the dest mountPoint for the device
+func getDeviceForPath(path string) (*device, error) {
+	if path == "" {
+		return &device{}, fmt.Errorf("Path cannot be empty")
+	}
+
+	stat := syscall.Stat_t{}
+	err := syscall.Stat(path, &stat)
+	if err != nil {
+		return &device{}, err
+	}
+
+	// stat.Dev points to the underlying device containing the file
+	major := major(stat.Dev)
+	minor := minor(stat.Dev)
+
+	mountPoint := path
+
+	// We get the mount point by recursively peforming stat on the path
+	// The point where the device changes indicates the mountpoint
+	for {
+		if mountPoint == "/" {
+			break
+		}
+
+		parentStat := syscall.Stat_t{}
+		parentDir := filepath.Dir(path)
+
+		err := syscall.Lstat(parentDir, &parentStat)
+		if err != nil {
+			return &device{}, err
+		}
+
+		if parentStat.Dev != stat.Dev {
+			break
+		}
+
+		mountPoint = parentDir
+		stat = parentStat
+		path = parentDir
+	}
+
+	dev := &device{
+		major:      major,
+		minor:      minor,
+		mountPoint: mountPoint,
+	}
+
+	return dev, nil
+}
---
+++ mount_test.go
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package virtcontainers
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
+)
+
+func TestMajorMinorNumber(t *testing.T) {
+	devices := []string{"/dev/zero", "/dev/net/tun"}
+
+	for _, device := range devices {
+		cmdStr := fmt.Sprintf("ls -l %s | awk '{print $5$6}'", device)
+		cmd := exec.Command("sh", "-c", cmdStr)
+		output, err := cmd.Output()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data := bytes.Split(output, []byte(","))
+		if len(data) < 2 {
+			t.Fatal()
+		}
+
+		majorStr := strings.TrimSpace(string(data[0]))
+		minorStr := strings.TrimSpace(string(data[1]))
+
+		majorNo, err := strconv.Atoi(majorStr)
+		minorNo, err := strconv.Atoi(minorStr)
+
+		stat := syscall.Stat_t{}
+		err = syscall.Stat(device, &stat)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Get major and minor numbers for the device itself. Note the use of stat.Rdev instead of Dev.
+		major := major(stat.Rdev)
+		minor := minor(stat.Rdev)
+
+		if minor != minorNo {
+			t.Fatalf("Expected minor number for device %s: %d, Got :%d", device, minorNo, minor)
+		}
+
+		if major != majorNo {
+			t.Fatalf("Expected major number for device %s : %d, Got :%d", device, majorNo, major)
+		}
+	}
+}
+
+func TestGetDeviceForPathRoot(t *testing.T) {
+	dev, err := getDeviceForPath("/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "/"
+
+	if dev.mountPoint != expected {
+		t.Fatalf("Expected %s mountpoint, got %s", expected, dev.mountPoint)
+	}
+}
+
+func TestGetDeviceForPathSuccess(t *testing.T) {
+	dev, err := getDeviceForPath("/proc")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "/proc"
+
+	if dev.mountPoint != expected {
+		t.Fatalf("Expected %s mountpoint, got %s", expected, dev.mountPoint)
+	}
+}
+
+func TestGetDeviceForPathEmptyPath(t *testing.T) {
+	_, err := getDeviceForPath("")
+	if err == nil {
+		t.Fatal()
+	}
+}
