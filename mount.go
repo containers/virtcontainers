d4346708a065dbd7f00878844666d8cc5d4f7aa9
Archana Shinde <archana.m.shinde@intel.com>
2017-06-06 01:13:50 -0700

    storage: Add function to get the underlying device and mount point
    Add helper function in preparation for using devicemapper block
    device. This function is used for getting the device major and
    minor number for the underlying device containing the file
    and the mountpoint of the mount containing the file.
    
    We will be using this for getting the device major-minor associated
    with the container rootfs and the mountPoint of the rootfs.
    
    We will eventually use the device to verify if it is a block device
    associated with devicemapper.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- mount.go
+++ mount.go
@@ -16,7 +16,13 @@
 
 package virtcontainers
 
-import "strings"
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"syscall"
+)
 
 // These mounts need to be created by the agent within the VM
 var systemMounts = []string{"/proc", "/dev", "/dev/pts", "/dev/shm", "/dev/mqueue", "/sys", "/sys/fs/cgroup"}
@@ -31,4 +37,93 @@ func isSystemMount(m string) bool {
 	}
 
 	return false
+}
+
+func major(dev uint64) int {
+	return int((dev >> 8) & 0xfff)
+}
+
+func minor(dev uint64) int {
+	return int((dev & 0xff) | ((dev >> 12) & 0xfff00))
+}
+
+type device struct {
+	major      int
+	minor      int
+	mountPoint string
+}
+
+var errMountPointNotFound = errors.New("Mount point not found")
+
+// getDeviceForPath gets the underlying device containing the file specified by path.
+// The device type constitutes the major-minor number of the device and the dest mountPoint for the device
+//
+// eg. if /dev/sda1 is mounted on /a/b/c, a call to getDeviceForPath("/a/b/c/file") would return
+//
+//	device {
+//		major : major(/dev/sda1)
+//		manor : minor(/dev/sda1)
+//		mountPoint: /a/b/c
+//	}
+func getDeviceForPath(path string) (device, error) {
+	if path == "" {
+		return device{}, fmt.Errorf("Path cannot be empty")
+	}
+
+	stat := syscall.Stat_t{}
+	err := syscall.Stat(path, &stat)
+	if err != nil {
+		return device{}, err
+	}
+
+	// stat.Dev points to the underlying device containing the file
+	major := major(stat.Dev)
+	minor := minor(stat.Dev)
+
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return device{}, err
+	}
+
+	mountPoint := path
+
+	if path == "/" {
+		return device{
+			major:      major,
+			minor:      minor,
+			mountPoint: mountPoint,
+		}, nil
+	}
+
+	// We get the mount point by recursively peforming stat on the path
+	// The point where the device changes indicates the mountpoint
+	for {
+		if mountPoint == "/" {
+			return device{}, errMountPointNotFound
+		}
+
+		parentStat := syscall.Stat_t{}
+		parentDir := filepath.Dir(path)
+
+		err := syscall.Lstat(parentDir, &parentStat)
+		if err != nil {
+			return device{}, err
+		}
+
+		if parentStat.Dev != stat.Dev {
+			break
+		}
+
+		mountPoint = parentDir
+		stat = parentStat
+		path = parentDir
+	}
+
+	dev := device{
+		major:      major,
+		minor:      minor,
+		mountPoint: mountPoint,
+	}
+
+	return dev, nil
 }
--- mount_test.go
+++ mount_test.go
@@ -16,7 +16,17 @@
 
 package virtcontainers
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
+)
 
 func TestIsSystemMount(t *testing.T) {
 	tests := []struct {
@@ -39,5 +49,158 @@ func TestIsSystemMount(t *testing.T) {
 		if result != test.expected {
 			t.Fatalf("Expected result for path %s : %v, got %v", test.mnt, test.expected, result)
 		}
+	}
+}
+
+func TestMajorMinorNumber(t *testing.T) {
+	devices := []string{"/dev/zero", "/dev/net/tun"}
+
+	for _, device := range devices {
+		cmdStr := fmt.Sprintf("ls -l %s | awk '{print $5$6}'", device)
+		cmd := exec.Command("sh", "-c", cmdStr)
+		output, err := cmd.Output()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data := bytes.Split(output, []byte(","))
+		if len(data) < 2 {
+			t.Fatal()
+		}
+
+		majorStr := strings.TrimSpace(string(data[0]))
+		minorStr := strings.TrimSpace(string(data[1]))
+
+		majorNo, err := strconv.Atoi(majorStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		minorNo, err := strconv.Atoi(minorStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		stat := syscall.Stat_t{}
+		err = syscall.Stat(device, &stat)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Get major and minor numbers for the device itself. Note the use of stat.Rdev instead of Dev.
+		major := major(stat.Rdev)
+		minor := minor(stat.Rdev)
+
+		if minor != minorNo {
+			t.Fatalf("Expected minor number for device %s: %d, Got :%d", device, minorNo, minor)
+		}
+
+		if major != majorNo {
+			t.Fatalf("Expected major number for device %s : %d, Got :%d", device, majorNo, major)
+		}
+	}
+}
+
+func TestGetDeviceForPathRoot(t *testing.T) {
+	dev, err := getDeviceForPath("/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "/"
+
+	if dev.mountPoint != expected {
+		t.Fatalf("Expected %s mountpoint, got %s", expected, dev.mountPoint)
+	}
+}
+
+func TestGetDeviceForPathValidMount(t *testing.T) {
+	dev, err := getDeviceForPath("/proc")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "/proc"
+
+	if dev.mountPoint != expected {
+		t.Fatalf("Expected %s mountpoint, got %s", expected, dev.mountPoint)
+	}
+}
+
+func TestGetDeviceForPathEmptyPath(t *testing.T) {
+	_, err := getDeviceForPath("")
+	if err == nil {
+		t.Fatal()
+	}
+}
+
+func TestGetDeviceForPath(t *testing.T) {
+	dev, err := getDeviceForPath("///")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if dev.mountPoint != "/" {
+		t.Fatal(err)
+	}
+
+	_, err = getDeviceForPath("/../../.././././../.")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = getDeviceForPath("/root/file with spaces")
+	if err == nil {
+		t.Fatal()
+	}
+}
+
+func TestGetDeviceForPathBindMount(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip(testDisabledAsNonRoot)
+	}
+
+	source := filepath.Join(testDir, "testDeviceDirSrc")
+	dest := filepath.Join(testDir, "testDeviceDirDest")
+	syscall.Unmount(dest, 0)
+	os.Remove(source)
+	os.Remove(dest)
+
+	err := os.MkdirAll(source, mountPerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(source)
+
+	err = os.MkdirAll(dest, mountPerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(dest)
+
+	err = bindMount(source, dest, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer syscall.Unmount(dest, 0)
+
+	destFile := filepath.Join(dest, "test")
+	_, err = os.Create(destFile)
+	if err != nil {
+		fmt.Println("Could not create test file:", err)
+		t.Fatal(err)
+	}
+
+	defer os.Remove(destFile)
+
+	sourceDev, _ := getDeviceForPath(source)
+	destDev, _ := getDeviceForPath(destFile)
+
+	if sourceDev != destDev {
+		t.Fatal()
 	}
 }
