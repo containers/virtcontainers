c92817b93c181310169bdc914ad3229675527aab
Archana Shinde <archana.m.shinde@intel.com>
2017-06-06 03:48:37 -0700

    storage: Pass the drive name and fstype to hyperstart
    We set the fstype in case a devicemapper block device is
    identified for a container. If this is set, pass it to
    hyperstart along with the drive name. The drive name
    is assigned in the order of insertion in the
    hypervisor  command line.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- hyperstart.go
+++ hyperstart.go
@@ -553,6 +553,8 @@ func (h *hyper) startPauseContainer(podID string) error {
 	return nil
 }
 
+var diskIndex = int(0)
+
 func (h *hyper) startOneContainer(pod Pod, c Container) error {
 	process, err := h.buildHyperContainerProcess(c.config.Cmd)
 	if err != nil {
@@ -564,6 +566,17 @@ func (h *hyper) startOneContainer(pod Pod, c Container) error {
 		Image:   c.id,
 		Rootfs:  rootfsDir,
 		Process: process,
+	}
+
+	if c.fstype != "" {
+		driveName, err := getVirtDriveName(diskIndex)
+		if err != nil {
+			return err
+		}
+
+		container.Fstype = c.fstype
+		container.Image = driveName
+		diskIndex = diskIndex + 1
 	}
 
 	if err := h.bindMountContainerRootfs(pod.id, c.id, c.rootFs, c.config.ReadonlyRootfs); err != nil {
