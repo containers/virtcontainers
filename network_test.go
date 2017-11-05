ad929287eb4b7ce2fbea7c1bcd329ffc471fe20f
Archana Shinde <archana.m.shinde@intel.com>
2017-11-05 13:27:37 -0800

    tests: Add test to check if interface is physical.
    Create a bridge in a new network namespace and verify if the
    test to check if the bridge is a physical interface returns false.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- network_test.go
+++ network_test.go
@@ -305,6 +305,63 @@ func TestGetIfacesFromNetNsFailureEmptyNetNsPath(t *testing.T) {
 	}
 }
 
+func TestIsPhysicalIface(t *testing.T) {
+	testNetIface := "testIface0"
+	testMTU := 1500
+	testMACAddr := "00:00:00:00:00:01"
+
+	hwAddr, err := net.ParseMAC(testMACAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	link := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:         testNetIface,
+			MTU:          testMTU,
+			HardwareAddr: hwAddr,
+			TxQLen:       -1,
+		},
+	}
+
+	n, err := ns.NewNS()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer n.Close()
+
+	netnsHandle, err := netns.GetFromPath(n.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer netnsHandle.Close()
+
+	netlinkHandle, err := netlink.NewHandleAt(netnsHandle)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer netlinkHandle.Delete()
+
+	if err := netlinkHandle.LinkAdd(link); err != nil {
+		t.Fatal(err)
+	}
+
+	var isPhysical bool
+	err = doNetNS(n.Path(), func(_ ns.NetNS) error {
+		var err error
+		isPhysical, err = isPhysicalIface(testNetIface)
+		return err
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if isPhysical == true {
+		t.Fatalf("Got %+v\nExpecting %+v", isPhysical, false)
+	}
+}
+
 func testGetIfacesFromNetNsSuccessful(t *testing.T, link netlink.Link, expected []netIfaceAddrs) {
 	lAttrs := link.Attrs()
 
