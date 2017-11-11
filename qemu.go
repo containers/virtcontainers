60266030cbd0ab60dc8408373f620e709718b024
Sebastien Boeuf <sebastien.boeuf@intel.com>
2017-11-10 16:13:32 -0800

    qemu: Don't wait for the end of the VM
    This patch brings in a real improvement about the time it takes
    to delete the pod. This is because it does not wait anymore for
    the end of the VM. Indeed, as long as we have called into QMP to
    shutdown the VM, this is not Clear Containers responsibility if
    the VM cannot be stopped for any reason. It basically should not
    happen.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- qemu.go
+++ qemu.go
@@ -803,7 +803,6 @@ func (q *qemu) waitPod(timeout int) error {
 func (q *qemu) stopPod() error {
 	cfg := ciaoQemu.QMPConfig{Logger: newQMPLogger()}
 	disconnectCh := make(chan struct{})
-	const timeout = time.Duration(10) * time.Second
 
 	q.Logger().Info("Stopping Pod")
 	qmp, _, err := ciaoQemu.QMPStart(q.qmpControlCh.ctx, q.qmpControlCh.path, cfg, disconnectCh)
@@ -818,19 +817,7 @@ func (q *qemu) stopPod() error {
 		return err
 	}
 
-	if err := qmp.ExecuteQuit(q.qmpMonitorCh.ctx); err != nil {
-		return err
-	}
-
-	// Wait for the VM disconnection notification
-	select {
-	case <-disconnectCh:
-		break
-	case <-time.After(timeout):
-		return fmt.Errorf("Did not receive the VM disconnection notification (timeout %ds)", timeout)
-	}
-
-	return nil
+	return qmp.ExecuteQuit(q.qmpMonitorCh.ctx)
 }
 
 func (q *qemu) togglePausePod(pause bool) error {
