f6922ae85ea2f52926b8c631b2a5d2ab14472e9a
Samuel Ortiz <sameo@linux.intel.com>
2017-12-14 01:26:48 +0100

    kata_agent: Create and start container implementations
    Create adds the rootfs and all container mounts.
    It also sets the OCI rootfs path properly.
    
    Start simply sends the StartContainer gRPC command.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- kata_agent.go
+++ kata_agent.go
@@ -18,20 +18,28 @@ package virtcontainers
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"syscall"
 
+	vcAnnotations "github.com/containers/virtcontainers/pkg/annotations"
+
 	kataclient "github.com/kata-containers/agent/protocols/client"
 	"github.com/kata-containers/agent/protocols/grpc"
+
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-var errorMissingGRPClient = fmt.Errorf("Missing gRPC client")
+var errorMissingGRPClient = errors.New("Missing gRPC client")
+var errorMissingOCISpec = errors.New("Missing OCI specification")
 var defaultHostSharedDir = "/tmp/kata-containers/shared/pods/"
 var defaultGuestSharedDir = "/tmp/kata-containers/shared/pods/"
 var mountGuest9pTag = "kataShared"
 var type9pFs = "9p"
+var devPath = "/dev"
 
 // KataAgentConfig is a structure storing information needed
 // to reach the Kata Containers agent.
@@ -153,12 +161,129 @@ func (k *kataAgent) stopPod(pod Pod) error {
 	return err
 }
 
+func appendStorageFromMounts(storage []*grpc.Storage, mounts []*Mount) []*grpc.Storage {
+	for _, m := range mounts {
+		s := &grpc.Storage{
+			Source:     m.Source,
+			MountPoint: m.Destination,
+		}
+
+		storage = append(storage, s)
+	}
+
+	return storage
+}
+
 func (k *kataAgent) createContainer(pod *Pod, c *Container) error {
-	return nil
+	if k.client == nil {
+		return errorMissingGRPClient
+	}
+
+	ociSpecJson, ok := c.config.Annotations[vcAnnotations.ConfigJSONKey]
+	if !ok {
+		return errorMissingOCISpec
+	}
+
+	var ociSpec specs.Spec
+	if err := json.Unmarshal([]byte(ociSpecJson), &ociSpec); err != nil {
+		return err
+	}
+
+	grpcSpec, err := grpc.OCItoGRPC(&ociSpec)
+	if err != nil {
+		return err
+	}
+
+	var containerStorage []*grpc.Storage
+
+	// The rootfs storage volume represents the container rootfs
+	// mount point inside the guest.
+	// It can be a block based device (when using block based container
+	// overlay on the host) mount or a 9pfs one (for all other overlay
+	// implementations).
+	rootfs := &grpc.Storage{}
+
+	// First we need to give the OCI spec our absolute path in the guest.
+	grpcSpec.Root.Path = filepath.Join(defaultGuestSharedDir, pod.id, c.id, rootfsDir)
+
+	if c.state.Fstype != "" {
+		// This is a block based device rootfs.
+		// driveName is the predicted virtio-block guest name (the vd* in /dev/vd*).
+		driveName, err := getVirtDriveName(c.state.BlockIndex)
+		if err != nil {
+			return err
+		}
+
+		rootfs.Source = filepath.Join(devPath, driveName)
+		rootfs.MountPoint = grpcSpec.Root.Path // Should we remove the "rootfs" suffix?
+		rootfs.Fstype = c.state.Fstype
+
+		// Add rootfs to the list of container storage.
+		// We only need to do this for block based rootfs, as we
+		// want the agent to mount it into the right location
+		// (/tmp/kata-containers/shared/pods/podID/ctrID/
+		containerStorage = append(containerStorage, rootfs)
+
+	} else {
+		// This is not a block based device rootfs.
+		// We are going to bind mount it into the 9pfs
+		// shared drive between the host and the guest.
+		// With 9pfs we don't need to ask the agent to
+		// mount the rootfs as the shared directory
+		// (/tmp/kata-containers/shared/pods/) is already
+		// mounted in the guest. We only need to mount the
+		// rootfs from the host and it will show up in the guest.
+		if err := bindMountContainerRootfs(defaultHostSharedDir, pod.id, c.id, c.rootFs, false); err != nil {
+			bindUnmountAllRootfs(defaultHostSharedDir, *pod)
+			return err
+		}
+	}
+
+	// Handle container mounts
+	newMounts, err := bindMountContainerMounts(defaultHostSharedDir, pod.id, c.id, c.mounts)
+	if err != nil {
+		bindUnmountAllRootfs(defaultHostSharedDir, *pod)
+		return err
+	}
+	containerStorage = appendStorageFromMounts(containerStorage, newMounts)
+
+	// Append container mounts for block devices passed with --device.
+	for _, device := range c.devices {
+		d, ok := device.(*BlockDevice)
+
+		if !ok {
+			continue
+		}
+
+		deviceStorage := &grpc.Storage{
+			Source:     d.VirtPath,
+			MountPoint: d.DeviceInfo.ContainerPath,
+		}
+
+		containerStorage = append(containerStorage, deviceStorage)
+	}
+
+	req := &grpc.CreateContainerRequest{
+		ContainerId: c.id,
+		Storages:    containerStorage,
+		OCI:         grpcSpec,
+	}
+
+	_, err = k.client.CreateContainer(context.Background(), req)
+	return err
 }
 
 func (k *kataAgent) startContainer(pod Pod, c Container) error {
-	return nil
+	if k.client == nil {
+		return errorMissingGRPClient
+	}
+
+	req := &grpc.StartContainerRequest{
+		ContainerId: c.id,
+	}
+
+	_, err := k.client.StartContainer(context.Background(), req)
+	return err
 }
 
 func (k *kataAgent) stopContainer(pod Pod, c Container) error {
